1fafefe53180f825941d295633fc8fd64ba00ec2
Orlando Richards <orichards@pixitmedia.com>
2021-02-26 11:19:51 +0000

    Update requirements parsing to work with modern pip

--- setup.py
+++ setup.py
@@ -1,18 +1,10 @@
 """setup.py file."""
-import uuid
-try:  # for pip >= 10
-    from pip._internal.req import parse_requirements
-except ImportError:  # for pip <= 9.0.3
-    from pip.req import parse_requirements
-
 from setuptools import find_packages, setup
-
 
 __author__ = 'Anas Badaha <anasb@mellanox.com'
 
-install_reqs = parse_requirements('requirements.txt', session=uuid.uuid1())
-reqs = [str(ir.req) for ir in install_reqs]
-
+with open("requirements.txt") as reqs_file:
+    reqs = reqs_file.readlines()
 
 setup(
     name="napalm-onyx",
